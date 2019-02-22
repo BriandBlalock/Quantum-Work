5e6b187934da3bded2075037683cab7d6faa7993
Briandblalock <Briandblalock@gmail.com>
2019-02-21 22:34:25 -0500

    work so far

---
+++ mutualUnbiased.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+import  numpy.linalg as la
+
+px = np.array([[0,1],[1,0]], np.complex_)
+py = np.array([[0,-1j],[1j,0]], np.complex_)
+pz = np.array([[1,0],[0,-1]], np.complex_)
